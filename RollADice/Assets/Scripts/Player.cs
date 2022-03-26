08b85c03e1cf5792de01e7941f838aa64e45fb3d
JHKWK <kjh2s@naver.com>
2022-03-26 18:48:10 +0900

    03.26
    너무 덥다

---
+++ RollADice/Assets/Scripts/Player.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public static Player instance; //싱글턴패턴 DicePlayManager에서 참조
+    private void Awake()
+    {
+        instance = this;
+    }
+    public void Move(Vector3 target) =>
+        transform.position = target;
+    
+}
